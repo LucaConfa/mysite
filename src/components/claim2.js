adfe3a6b04a82fbe11e6cce549510440b359c541
luca <luca.confa80@gmail.com>
2023-08-02 16:14:36 +1000

    prevent zoom on safari

---
+++ src/components/claim2.js
@@ -0,0 +1,25 @@
+import { Title } from './title';
+import gumgumMR_logo from '../gumgumMR_logo.png';
+
+export const Claim2 = (props) => {
+  const { onClick } = props;
+  return (
+    <>
+      <img
+        style={{ width: '88px', height: '100%' }}
+        src={gumgumMR_logo}
+        alt="Gumgum Market Research Logo"
+      />
+      <Title text="Tap to claim your $10 reward" />
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          onClick('tremendous');
+        }}
+      >
+        https://reward.tremendous.com/rewards/hyc0hzivc--mxcdiyshpqwvh5gtdiko9vzuw-h3lxni
+      </a>
+    </>
+  );
+};
